7a15129d6b68e19de9894fa7834b0cfa7901e493
Erkan Domurcuk <erkan@edomurcuk.com>
2020-11-12 19:33:33 +0300

    Add project files.

---
+++ DenemeProjemiz.Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using DenemeProjemiz.Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq.Expressions;
+
+namespace DenemeProjemiz.Data
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        int Insert(T entity);
+        int InsertRange(IEnumerable<T> list);
+        int Update(T entity);
+        int Delete(int id, bool? realDelete = null);
+        T Get(int id);
+        IEnumerable<T> GetList(Expression<Func<T, bool>> filter = null);
+        IEnumerable<T> GetCustomQuery(string customQuery, DynamicParameters parameters, CommandType commandType);
+        IEnumerable<T> GetAllIds(string fieldName, List<int> Id);
+        long GetList_Count(Expression<Func<T, bool>> filter = null);
+        string GetCustomQueryToString(string customQuery, DynamicParameters parameters, CommandType commandType);
+    }
+}
---
+++ DenemeProjemiz.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DenemeProjemiz.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
